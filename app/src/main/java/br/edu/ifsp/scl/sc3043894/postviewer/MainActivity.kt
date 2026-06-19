ae1257f398becca16502aa10871485dce0336293
montillha <marianaalmeida235b@gmail.com>
2026-06-18 21:35:41 -0300

    feat: wire navigation and top app bar into MainActivity

--- app/src/main/java/br/edu/ifsp/scl/sc3043894/postviewer/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3043894/postviewer/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import br.edu.ifsp.scl.sc3043894.postviewer.ui.composable.MainNavHost
+import br.edu.ifsp.scl.sc3043894.postviewer.ui.composable.MainTopAppBar
 import br.edu.ifsp.scl.sc3043894.postviewer.ui.theme.PostViewerTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,10 +21,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController: NavHostController = rememberNavController()
+            val postViewModel: PostViewModel = viewModel()
             PostViewerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                Scaffold(
+                    topBar = { MainTopAppBar() },
+                    modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainNavHost(
+                        navController = navController,
+                        postViewModel = postViewModel,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +38,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PostViewerTheme {
-        Greeting("Android")
-    }
-}
