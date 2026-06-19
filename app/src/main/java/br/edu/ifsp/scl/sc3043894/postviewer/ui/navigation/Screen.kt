263e32f644e92e3d6d97789eafc2d52af0cb0fec
montillha <marianaalmeida235b@gmail.com>
2026-06-18 21:43:44 -0300

    fix: correct postId argument name mismatch in details route

--- app/src/main/java/br/edu/ifsp/scl/sc3043894/postviewer/ui/navigation/Screen.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3043894/postviewer/ui/navigation/Screen.kt
@@ -2,7 +2,7 @@ package br.edu.ifsp.scl.sc3043894.postviewer.ui.navigation
 
 sealed class Screen(val route: String) {
     object Posts : Screen(route = "posts")
-    object Details : Screen(route = "details/{postsId}"){
+    object Details : Screen(route = "details/{postId}"){
         fun createRoute(postId: Int) = "details/$postId"
     }
 }
